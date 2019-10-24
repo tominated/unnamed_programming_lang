49da22207259bfa79e9e6d417f78fa1eb6546c4f
John Rodriguez <jrodriguez.0987+github@gmail.com>
2019-10-24 10:55:20 -0400

    Add type-checking to repl
    Co-authored-by: jrdrg <jrodriguez.0987+github@gmail.com>
    Co-authored-by: tominated <tominated@users.noreply.github.com>

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,27 @@
 open Base
 open Parser
 open Ast
+open Typechecker
 
 let parse lexbuf =
   match (Parser.parse_expression lexbuf) with
-  | Ok e -> Syntax.expression_to_string e
-  | Error msg -> msg
+  | Ok e -> 
+    (match (Infer.infer_type Infer.Env.empty e) with
+    | Ok t -> 
+      let expr_str = Syntax.expression_to_string e in 
+      let type_str = Syntax.type_signature_to_string t in
+      Stdio.printf "%s\n%s\n" expr_str type_str
+    | Error err -> Stdio.print_endline (Infer.err_to_string err)
+    ) 
+
+  | Error msg -> msg |> Stdio.print_endline
 
 let rec loop () =
   Stdio.printf "Enter Exp: %!";
   match Stdio.In_channel.input_line Stdio.stdin with
   | None | Some "exit" | Some "quit" -> ()
   | Some s ->
-    parse (Sedlexing.Utf8.from_string s) |> Stdio.print_endline;
+    parse (Sedlexing.Utf8.from_string s);
     loop ()
 
 let () = loop ()
